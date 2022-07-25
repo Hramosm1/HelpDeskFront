0c5ff184c05da22ea50a97334d6bfa9c44b97275
Diego Jared  Donis Soto <programacion4@recagua.com>
2022-07-25 13:36:59 -0600

    comentarios en tiempo real

--- src/app/modules/tickets/comments/comments.component.ts
+++ src/app/modules/tickets/comments/comments.component.ts
@@ -3,8 +3,9 @@ import { FormControl, Validators } from '@angular/forms';
 import { HttpService } from 'app/backend/services/http.service';
 import { quillConfig } from 'app/core/config/quill.config';
 import { UserService } from 'app/core/user/user.service';
+import { SocketsService } from 'app/shared/services/sockets.service';
 import { QuillEditorComponent } from 'ngx-quill';
-import { Observable, Subject, switchMap } from 'rxjs';
+import { mergeMap, Observable, Subject, Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-comments',
@@ -19,7 +20,8 @@ export class CommentsComponent implements OnInit, AfterViewInit, OnDestroy {
   comentarios$: Observable<any>
   comentario = new FormControl('', Validators.required)
   usuario: string
-  constructor(private api: HttpService, private _user: UserService) { }
+  subs: Subscription
+  constructor(private api: HttpService, private _user: UserService, private socket: SocketsService) { }
 
 
   ngOnInit(): void {
@@ -31,9 +33,11 @@ export class CommentsComponent implements OnInit, AfterViewInit, OnDestroy {
   }
   ngAfterViewInit(): void {
     this.subject$.next(null)
+    this.subs = this.socket.nuevoComentario$.subscribe(_ => this.subject$.next(null))
   }
   ngOnDestroy(): void {
     this.subject$.complete()
+    this.subs.unsubscribe()
   }
   submit() {
     const body = { idTicket: Number(this.ticket), comentario: this.comentario.value, idUsuario: this.usuario }
--- src/app/shared/services/sockets.service.ts
+++ src/app/shared/services/sockets.service.ts
@@ -7,7 +7,19 @@ import { Observable, Subject } from 'rxjs';
 })
 export class SocketsService {
   nuevoTicket$: Observable<any> = this.getEvents$()
+  nuevoComentario$: Observable<any> = this.getComentarios()
   constructor(private socket: Socket) {
+  }
+  private getComentarios() {
+    return new Observable(observer => {
+      try {
+        this.socket.on('nuevoComentario', (data) => { observer.next(data) })
+        this.socket.on('error', (e) => { observer.error(e) })
+        this.socket.on('disconnect', () => { observer.next() })
+      } catch (error) {
+        observer.error(error)
+      }
+    })
   }
   private getEvents$() {
     return new Observable(observer => {
