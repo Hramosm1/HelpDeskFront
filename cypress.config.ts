52e83be36dfa930c5f4ded0ba8bdea790434eca7
Diego Jared  Donis Soto <programacion4@recagua.com>
2022-07-26 16:50:26 -0600

    primeras pruebas e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    'baseUrl': 'http://localhost:4200',
+    'baseUrl': 'http://192.168.8.28:9410',
   },
 })
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,20 @@
-describe('/sign-in', () => {
+describe('LOGIN', () => {
   it('carga la pantalla', () => {
-    cy.visit('/sign-in')
+    cy.visit('/')
+    cy.contains('Inicia session')
+  })
+  it('no ingresa con credenciales incorrectas', () => {
+    cy.visit('/')
+    cy.get('#user').type('dgf')
+    cy.get('#password').type('f5g4')
+    cy.get('.fuse-mat-button-large').click()
+    cy.url().should('include', '/sign-in')
+  })
+  it('inicia session con las credenciales correctas', () => {
+    cy.visit('/')
+    cy.get('#user').type('donis')
+    cy.get('#password').type('123456')
+    cy.get('.fuse-mat-button-large').click()
+    cy.url().should('include', 'tickets/list')
   })
 })
