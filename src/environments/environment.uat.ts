249f450b04e2a2fd591ea491742c518e3fd2e9b1
Diego Jared Donis Soto <64775200+DiegoDoniss@users.noreply.github.com>
2022-08-30 16:25:29 -0600

    pwa

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
   backenduri: 'http://192.168.8.28:9411/',
-  autenticacionuri: 'http://192.168.8.28:9401/'
+  autenticacionuri: 'http://192.168.8.28:9401/',
+	publicKey: 'BD5m2Sk_dsAHfMskblN_EGdhht5_MH3ZQIt0PAO_XM26sdkPoMA65RKTFE9QunYv-x2qF-U4YB4gPp-kJC2eChc',
+	privateKey: 'cOCAt66UhA24vaiWarHaYVwE4NybDURL-peoB2zSRH8'
 };
